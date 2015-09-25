369abc9d4719402fe11bcaba73e6affcd5f6909b
ajdennis78 <ajdennis@gmail.com>
2015-09-25 12:30:43 -0700

    Removing PHP and Python

--- startPP.sh
+++ startPP.sh
@@ -29,13 +29,13 @@ cleanUp() {
 }
 trap cleanUp EXIT
 
-echo "Start Partner Catalog Service (PHP)"
-# Start Partner Catalog container - PHP
-docker run -d --name partner_catalog_service \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e SSL=${SSL} \
-	-e APP_NAME=${PP_APP_NAME} -e PHP_TIER_NAME=${PHP_TIER_NAME} -e PHP_NODE_NAME=${PHP_NODE_NAME} \
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e SERVICE_URL=${SERVICE_URL} \
-	--link web:web appdynamics/partner-catalog-service:latest
+#echo "Start Partner Catalog Service (PHP)"
+## Start Partner Catalog container - PHP
+#docker run -d --name partner_catalog_service \
+#	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e SSL=${SSL} \
+#	-e APP_NAME=${PP_APP_NAME} -e PHP_TIER_NAME=${PHP_TIER_NAME} -e PHP_NODE_NAME=${PHP_NODE_NAME} \
+#	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} -e SERVICE_URL=${SERVICE_URL} \
+#	--link web:web appdynamics/partner-catalog-service:latest
 
 echo "Start Customer Survey Service (Node.js)"
 # Start Partner Catalog container - PHP
@@ -45,20 +45,20 @@ docker run -d --name customer_survey_service \
 	-e APP_NAME=${PP_APP_NAME} -e TIER_NAME=${NODE_TIER_NAME} -e NODE_NAME=${NODE_NODE_NAME} \
 	--link web:web appdynamics/customer-survey-service:latest
 
-echo "Start Partner Portal App (Python)"
-# Start Python containers 
-echo -n "python_mysql: "; docker run -d --name python_mysql appdynamics/python-mysql:latest
-echo -n "python_postgresql: ";docker run -d --name python_postgresql appdynamics/python-postgresql:latest
-sleep 10
+#echo "Start Partner Portal App (Python)"
+## Start Python containers 
+#echo -n "python_mysql: "; docker run -d --name python_mysql appdynamics/python-mysql:latest
+#echo -n "python_postgresql: ";docker run -d --name python_postgresql appdynamics/python-postgresql:latest
+#sleep 10
 
-echo -n "python_app: ";docker run -d --name python_app \
-	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e SSL=${SSL}\
-	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
-	-e APP_NAME=${PP_APP_NAME} -e TIER_NAME=${PY_TIER_NAME} -e NODE_NAME=${PY_NODE_NAME} \
-	--link python_mysql:python_mysql --link python_postgresql:python_postgresql \
-	--link partner_catalog_service:partner_catalog_service --link customer_survey_service:customer_survey_service appdynamics/python-app:latest
-
-echo -n "python_siege: "; docker run -d --name python_siege -e BUNDY_TIER=${SIEGE_URL} -e PROCUREMENT_URL=${PROCUREMENT_URL}\
-	--link web:web --link python_app:python_app appdynamics/python-siege:latest
+#echo -n "python_app: ";docker run -d --name python_app \
+#	-e ACCOUNT_NAME=${ACCOUNT_NAME} -e ACCESS_KEY=${ACCESS_KEY} -e SSL=${SSL}\
+#	-e CONTROLLER=${CONTR_HOST} -e APPD_PORT=${CONTR_PORT} \
+#	-e APP_NAME=${PP_APP_NAME} -e TIER_NAME=${PY_TIER_NAME} -e NODE_NAME=${PY_NODE_NAME} \
+#	--link python_mysql:python_mysql --link python_postgresql:python_postgresql \
+#	--link partner_catalog_service:partner_catalog_service --link customer_survey_service:customer_survey_service appdynamics/python-app:latest
+#
+#echo -n "python_siege: "; docker run -d --name python_siege -e BUNDY_TIER=${SIEGE_URL} -e PROCUREMENT_URL=${PROCUREMENT_URL}\
+#	--link web:web --link python_app:python_app appdynamics/python-siege:latest
 
 exit 0
